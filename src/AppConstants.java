acc984bd2caee5fafcf982a997014817afa01716
Mister Brown <greendoggy@users.noreply.github.com>
2015-08-02 19:05:58 -0400

    received official values for Telegram

---
+++ src/AppConstants.java
@@ -0,0 +1,19 @@
+
+public class AppConstants {
+	public static final int APP_ID = 45002;
+	public static final String APP_HASH = "e887d2c1ed91b5b710d2730102db2ee0";
+	public static final String TEST_DC = "149.154.167.40:443";
+	public static final String PROUCTION_DC = "149.154.167.50:443";
+	
+	public static final String RSA_PUBKEY = "-----BEGIN RSA PUBLIC KEY-----"
+			+ "MIIBCgKCAQEAwVACPi9w23mF3tBkdZz+zwrzKOaaQdr01vAbU4E1pvkfj4sqDsm6"
+			+ "lyDONS789sVoD/xCS9Y0hkkC3gtL1tSfTlgCMOOul9lcixlEKzwKENj1Yz/s7daS"
+			+ "an9tqw3bfUV/nqgbhGX81v/+7RFAEd+RwFnK7a+XYl9sluzHRyVVaTTveB2GazTw"
+			+ "Efzk2DWgkBluml8OREmvfraX3bkHZJTKX4EQSjBbbdJ2ZXIsRrYOXfaA+xayEGB+"
+			+ "8hdlLmAjbCVfaigxX0CDqWeR1yFL9kwd9P0NsZRPsmoqVwMbMu7mStFai6aIhc3n"
+			+ "Slv8kg9qv1m6XHVQY3PnEw+QQtqSIXklHwIDAQAB"
+			+ "-----END RSA PUBLIC KEY-----";
+	
+	public static final String APP_TITLE = "Java Messages";
+	public static final String APP_SHORT_NAME = "JMessages";
+}
